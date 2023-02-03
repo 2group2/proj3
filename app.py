7c3fe6df44fc3b93f95524ee38281831907f5342
Paula Letícia Gerónimo <paulagero182@gmail.com>
2023-02-02 18:42:44 -0600

    app.py basic setup and task division

---
+++ app.py
@@ -0,0 +1,18 @@
+# library imports
+
+# variable initialization
+
+
+# TODO: retrieve log file 
+# (Two People)
+
+
+# TODO: calculating requests from first SIX months
+# (Two People)
+
+
+# TODO: calculating TOTAL amount of requests made
+
+
+# TODO: output for marketing
+
